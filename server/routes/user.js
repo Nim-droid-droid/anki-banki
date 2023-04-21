127dbd05fe3e20afb4ce01442f484d605355ecda
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-04-21 10:45:03 -0400

    Implement signup and login backend routes
    - Accept posts to /signup and /login endpoints
    - User model requires { username, email, password }, _id is generated by Mongo for each User
        - Signup requires { username, email, password, confirmPassword } in request body
        - Login requires { email, password } passed in request body

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router()
 const userController = require("../controllers/user")
 
-router.post('/login', userController.postLogin)
+router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.post('/signup', userController.signup)
 
