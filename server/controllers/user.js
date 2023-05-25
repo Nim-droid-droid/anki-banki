261a6d9aed31a63095b160d8af5d38c7de97f27b
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-05-25 17:32:49 -0400

    Add timestamps to User model

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
 const data = require("../models/index")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
-const User = require("../models/user")
+const User = require("../models/User")
 const dotenv = require("dotenv").config({ path: "./config/.env" });
 
 const login = async(req, res) => {
