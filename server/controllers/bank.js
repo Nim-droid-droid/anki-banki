9b345f1b2c69809ee72c0a7b23da86da2cf6b5ed
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-06-11 23:50:00 -0400

    Add question API filter parameters
    - Update questions.json to include some technical questions (html, css, javascript)
    with a `categories` property

--- server/controllers/bank.js
+++ server/controllers/bank.js
@@ -3,11 +3,23 @@ const question_data = require("../data/questions.json")
 module.exports = {
     getQuestions: async(req, res) => {
         try {
-            types = req.query.types;
-            data = question_data.filter(x => types.includes(x.type))
-            res.json(data)
+            let data = question_data;
+            const types = req.query.types;
+            if (types) {
+                console.log(types)
+                data = question_data.filter(x => types.includes(x.type))
+            }
+
+            const categories = new Array(req.query.categories);
+            console.log(categories)
+            if (req.query.categories) {
+                data = question_data.filter(item => categories.every(cat => item.categories.includes(cat)))
+            }
+
+            console.table(data)
+            res.json(data) 
         } catch (err) {
-            res.send("Bad question parameters")
+            res.send(`Poorly formed query: ${err}`)
 
         }
     }
