6eaaaccb08fecd7955a36a602bf58c8ccf862a62
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-06-30 17:59:52 -0400

    Add validators to login and signup routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/User")
 const passport = require("passport")
+const validator = require("validator")
 
 const getLogin = (req, res) => {
     res.send("Login page")
@@ -10,29 +11,51 @@ const getSignup = (req, res) => {
 }
 
 const postLogin = (req, res, next) => {
-    // TODO: email / password with validator.js
+    const validationErrors = []
+    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: "Please enter a valid email address." })
+    if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: "Password cannot be blank." })
+
+    if (validationErrors.length) {
+        req.flash("errors", validationErrors)
+        return res.redirect("/login")
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+
     passport.authenticate("local", { failureRedirect: "/login", failureMessage: true},
         (err, user, info) => {
             if (err) { return next(err) }
             if (!user) {
                 console.log(`Failed login. User: "${req.body.email}" does not exist`)
-                return res.redirect("/")
+                return res.redirect("/login")
             }
             req.login(user, (err) => {
                 if(err) { return next(err) }
                 console.log(`Successful login by: ${user.email}`)
-                res.redirect("/home")
+                req.flash("sucess", { msg: "Success! You are logged in ." })
+                res.redirect(req.session.returnTo || "/")
             })
         })(req, res, next)
 }
 
 const postSignup = async(req, res, next) => {
-    // TODO: sanitize username / email / password with validator.js
+    const validationErrors = []
+    const minPassLength = 8
+    // TODO: add stricter password requirements
+    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: "Please enter a valid email address." })
+    if (!validator.isLength(req.body.password, { min: minPassLength })) validationErrors.push({ msg: `Password must be at least ${minPassLength} characters`})
+    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: "Passwords do not match" })
+
+    if (validationErrors.length) {
+        req.flash("errors", validationErrors)
+        console.log(validationErrors)
+        return res.redirect("/signup")
+    }
+
     const { username, email, password, confirmPassword } = req.body;
 
     const user = new User({ username, email, password })
 
-    const existingUser = await User.findOne({$or: [ { email }, { username }]})
+    const existingUser = await User.findOne({$or: [{ email }, { username }]})
     if (existingUser) {
         console.log("Can't signup. Account already exists")
         return res.redirect("/signup")
@@ -59,12 +82,9 @@ const googleCallback = async(req, res) => {
 }
 
 const logout = (req, res) => {
-    // TODO: remove this workaround once passportjs is properly updated to stop giving
-    // req.session.regenerate undefined errors, otherwise this is a needed workaround
-    req.session.regenerate((cb) => null)
-
-    req.logout(() => {
-        console.log("User has logged out")
+    req.logout((err) => {
+        if (err) { return next (err) }
+        console.log(`Sucessful logout by ${req.user}`)
     })
 
     req.session.destroy((err) => {
@@ -83,6 +103,7 @@ const deleteUser = (req, res) => {
     res.send("deleteUser")
 }
 
+// TODO: inline exports
 module.exports = {
     getLogin,
     getSignup,
