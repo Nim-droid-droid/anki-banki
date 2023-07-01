20744325789d9a1abdfb9f386a70185adae073ef
umesh <umesharma.us11@gmail.com>
2023-07-01 21:59:08 +0530

    Created the UI for the filters

---
+++ client/src/pages/myBank/data.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    type: [
+      'Behaviourial',
+      'technical',
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Node',
+      'CS Theory',
+    ],
+  },
+  {
+    status: ['unanswered', 'answered', 'Great!', 'Good', 'OK', 'Not So Good'],
+  },
+];
+export default data;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors:{
-        primary:'#8EF3C3',
-        gray100:'#D9D9D9',
-        gray600:'#2F2F2F'
-      }
+      colors: {
+        primary: '#8EF3C3',
+        gray100: '#D9D9D9',
+        gray600: '#2F2F2F',
+        orange: '#FF9574',
+      },
     },
   },
   plugins: [],
-}
-
+};
